1d9bd3ba2695153b05bb06092325229923bc5500
YanaKovalI <yana.koval.de@gmail.com>
2024-01-01 20:01:36 +0100

    implemented main function

--- main.py
+++ main.py
@@ -14,45 +14,8 @@ from label_search_wikidata import SPARQLQueryDispatcher
 import json
 import pandas as pd
 import labelsdb_wiki
-
-
-def wikidata_main():
-    start_time = datetime.datetime.now().replace(microsecond=0)
-    stored_results = 'sparql_queries_results.json'
-    table1 = "data/example_dataset/countries0.csv"
-    table2 = "data/example_dataset/countries2.csv"
-    info1, info2 =  get_info_from_wikidata.get_info_with_check(stored_results_json=stored_results,table1=table1,table2=table2)
-    r = relatedness.get_average_pair(info1, info2)   
-    relatedness_between_sets = relatedness.get_relatedness_sets(info1, info2)
-    print("\n")
-    print("RESULT after {0} of calculation:".format(datetime.datetime.now().replace(microsecond=0) - start_time))
-    print("Average relatedness between " + str(table1) + " and " + str(table2) + ": " + str(relatedness_between_sets))
-
-
-
-def weighted_dbpedia_main():
-    start_time = datetime.datetime.now().replace(microsecond=0)
-    table1 = "data/example_dataset/countries8.csv"
-    table2 = "data/example_dataset/countries2.csv"
-    entities1 = extract_entities.extract_entities_from_table(table1)
-    entities2 = extract_entities.extract_entities_from_table(table2)
-    dbpedia_labels1 = dbpedia.get_weighted_labels(entities1)
-    dbpedia_labels2 = dbpedia.get_weighted_labels(entities2)
-    print(dbpedia_labels1)
-    print(dbpedia_labels2)
-    time_labels = datetime.datetime.now().replace(microsecond=0) - start_time
-    r = relatedness.get_average_pair(dbpedia_labels1, dbpedia_labels2)
-    r2 = relatedness.get_set_relatedness(dbpedia_labels1, dbpedia_labels2)
-    r3 = relatedness.get_set_relatedness_expansion(dbpedia_labels1, dbpedia_labels2)
-    time_calculation = datetime.datetime.now().replace(microsecond=0) - time_labels - start_time
-    print("\n")
-    print("RESULT after {0} of getting labels and {1} of calculating:".format(time_labels, time_calculation))
-    print("Average relatedness between " + str(table1) + " and " + str(table2) + ": " + str(r))
-    print("Average relatedness between " + str(table1) + " and " + str(table2) + ": " + str(r2))
-    print("Average relatedness between " + str(table1) + " and " + str(table2) + ": " + str(r3))
-
-
-def get_relatedness_for_multiple_tables_dbpedia(table1="data/example_dataset/countries8.csv"):
+         
+def get_relatedness_for_multiple_tables_dbpedia(table1="data/example_dataset/countries8.csv", order_path = 'data\\example_dataset', n1=1.0, n2=1.0, m1=1.0, m2=1.0):
     labelsdb.init_database()
 
     query_time = 0
@@ -63,12 +26,12 @@ def get_relatedness_for_multiple_tables_dbpedia(table1="data/example_dataset/cou
     table_relatedness = dict()
 
     dirname = os.path.dirname(__file__)
-    directory = os.path.join(dirname, 'data\\example_dataset')
+    directory = os.path.join(dirname, order_path)
     for root, dirs, files in os.walk(directory):
         file_number = 1
         for file in files:
             if file.endswith(".csv"):
-                filename = os.path.join('data\\example_dataset', file)
+                filename = os.path.join(order_path, file)
 
                 start = time.time()
 
@@ -78,7 +41,7 @@ def get_relatedness_for_multiple_tables_dbpedia(table1="data/example_dataset/cou
                 query_stop = time.time()
                 query_time += query_stop - start
 
-                r3 = relatedness.get_set_relatedness_expansion(dbpedia_labels1, dbpedia_labels2)
+                r3 = relatedness.get_set_relatedness_expansion(dbpedia_labels1, dbpedia_labels2, n1, n2, m1, m2)
                 table_relatedness[file] = r3
 
                 calculation_stop = time.time()
@@ -104,7 +67,7 @@ def get_relatedness_for_multiple_tables_dbpedia(table1="data/example_dataset/cou
         print("{0}: {1}".format(key, value))
 
 
-def get_relatedness_for_multiple_tables_wiki(table1="data/example_dataset/countries8.csv"):
+def get_relatedness_for_multiple_tables_wiki(table1="data/example_dataset/countries8.csv", order_path = 'data\\example_dataset', n1=1.0, n2=1.0, m1=1.0, m2=1.0):
     labelsdb_wiki.init_database()
     query_time = 0
     calculation_time = 0
@@ -115,12 +78,12 @@ def get_relatedness_for_multiple_tables_wiki(table1="data/example_dataset/countr
     table_relatedness = dict()
 
     dirname = os.path.dirname(__file__)
-    directory = os.path.join(dirname, 'data\\example_dataset')
+    directory = os.path.join(dirname, order_path)
     for root, dirs, files in os.walk(directory):
         file_number = 1
         for file in files:
             if file.endswith(".csv"):
-                filename = os.path.join('data\\example_dataset', file)
+                filename = os.path.join(order_path, file)
                 start = time.time()
 
                 entities2 = extract_entities.extract_entities_from_table(filename)
@@ -129,7 +92,7 @@ def get_relatedness_for_multiple_tables_wiki(table1="data/example_dataset/countr
                 query_stop = time.time()
                 query_time += query_stop - start
 
-                r3 = relatedness.get_set_relatedness_expansion(wikidata_weighted_labels1, wikidata_weighted_labels2)
+                r3 = relatedness.get_set_relatedness_expansion(wikidata_weighted_labels1, wikidata_weighted_labels2, n1, n2, m1, m2)
                 table_relatedness[file] = r3
 
                 calculation_stop = time.time()
@@ -153,10 +116,15 @@ def get_relatedness_for_multiple_tables_wiki(table1="data/example_dataset/countr
             break
         i += 1
         print("{0}: {1}".format(key, value))        
+        
+
+# MAIN FUNCTION
 
 
-get_relatedness_for_multiple_tables_wiki()
-# def main(input_table, order_path, n1, n2, m1, m2, quelle):
+def main(input_table, order_path, n1, n2, m1, m2, source):
+    if 'wikidata' == source.lower():
+        get_relatedness_for_multiple_tables_wiki(input_table, order_path,  n1=1.0, n2=1.0, m1=1.0, m2=1.0)
+    elif 'dbpedia' == source.lower():
+        get_relatedness_for_multiple_tables_dbpedia(input_table, order_path, n1=1.0, n2=1.0, m1=1.0, m2=1.0)   
 
-# entities = extract_entities.extract_entities_from_table("data/datalake/wholesale_markets_2.csv")
-# print(entities)
+main(input_table="data/example_dataset/countries8.csv", order_path = 'data\\example_dataset', n1=1.0, n2=1.0, m1=1.0, m2=1.0, source= "dbpedia") 
